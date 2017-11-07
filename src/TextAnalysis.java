492663c78082d8b1f69f4c4a06b9efd08bd73cc5
Dylan Vu <dylan.n.vu@gmail.com>
2017-11-07 03:05:43 -0800

    refactor to remove unnecessary code and improve performance

--- src/TextAnalysis.java
+++ src/TextAnalysis.java
@@ -4,94 +4,97 @@ import java.util.*;
 public class TextAnalysis {
     public static void main(String[] args)
             throws FileNotFoundException {
-        // store directory paths into File variable to loop over
+        // Store directory paths into File variable to loop over
         File worksByAusten = new File("./data/austen/");
         File worksByBronte = new File("./data/bronte/");
 
         printIntro();
 
-        // Find unique words in works of both authors
-        Set<String> uniqueWordsInWorksByAusten = getUniqueWords(worksByAusten);
-        Set<String> uniqueWordsInWorksByBronte = getUniqueWords(worksByBronte);
-
-        // Find unique words in Bronte's work not used in Austen's works
-        uniqueWordsInWorksByBronte.removeAll(uniqueWordsInWorksByAusten);
-
-        // Create map of word count using set values(String) as keys
-        Map<String, Integer> wordCountMap = convertSetToMapWithValue0(uniqueWordsInWorksByBronte);
-
         // Increment values based on .txt files
-        wordCountMap = getWordCount(wordCountMap, worksByBronte);
+        Map<String, Integer> austenWordCount = getWordCount(worksByAusten);
+        Map<String, Integer> bronteWordCount = getWordCount(worksByBronte, austenWordCount);
 
         // Print sorted list of hashMap values
-        printResults(convertHashMapToList(wordCountMap));
-
-
+        printResults(convertHashMapToList(bronteWordCount));
     }
 
-    public static void printIntro() {
+    private static void printIntro() {
         System.out.println("This program displays the top 10 words and");
         System.out.println("number of times they occur in books by Charlotte Bronte");
         System.out.println("but not used by Jane Austen");
         System.out.println();
     }
 
-    public static Set<String> getUniqueWords(File dir)
+    // Increment values in passed Map based on inputted .txt files
+    private static Map<String, Integer> getWordCount(File dir)
             throws FileNotFoundException {
-        Set<String> uniqueWords = new HashSet<String>();
 
-        // Loop over all files in given directory
-        for (File file : dir.listFiles()) {
-            // check for .txt extension
-            if (file.getName().endsWith(".txt")) {
-                Scanner input = new Scanner(file);
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        File[] files = dir.listFiles();
 
-                // parse words out of String
-                input.useDelimiter("[^a-zA-Z']+");
+        if (files == null) {
+            System.out.println("No files in directory");
+        }
+        else {
+            // Loop over all files in given directory
+            for (File file : dir.listFiles()) {
+                // check for .txt extension
+                if (file.getName().endsWith(".txt")) {
+                    Scanner input = new Scanner(file);
 
-                while (input.hasNext()) {
-                    // toLowerCase to prevent duplicate keys of same word but different case
-                    String word = input.next().toLowerCase();
-                    uniqueWords.add(word);
+                    // parse words out of String
+                    input.useDelimiter("[^a-zA-Z']+");
+
+                    while (input.hasNext()) {
+                        // toLowerCase to prevent duplicate keys of same word but different case
+                        String word = input.next().toLowerCase();
+
+                        if (wordCountMap.containsKey(word)) {
+                            int count = wordCountMap.get(word) + 1;
+                            wordCountMap.put(word, count);
+                        } else {
+                            wordCountMap.put(word, 1);
+                        }
+                    }
                 }
             }
         }
 
-        return uniqueWords;
+        return wordCountMap;
     }
 
-    // Create a wordCountMap with all values = 0 for each key
-    public static Map<String, Integer> convertSetToMapWithValue0(Set<String> set) {
-        Map<String, Integer> map = new TreeMap<String, Integer>();
-
-        // Set values for all new keys 0
-        for (String s : set) {
-            map.put(s, 0);
-        }
-
-        return map;
-    }
-
-    // Increment values in passed Map based on inputted .txt files
-    public static Map<String, Integer> getWordCount(Map<String, Integer> wordCountMap, File dir)
+    // Overload getWordCount to create map with values unique to one data set
+    private static Map<String, Integer> getWordCount(File dir, Map<String, Integer> mapToCompare)
             throws FileNotFoundException {
 
-        // Loop over all files in given directory
-        for(File file : dir.listFiles()) {
-            // check for .txt extension
-            if (file.getName().endsWith(".txt")) {
-                Scanner input = new Scanner(file);
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        File[] files = dir.listFiles();
 
-                // parse words out of String
-                input.useDelimiter("[^a-zA-Z']+");
+        if (files == null) {
+            System.out.println("No files in directory");
+        }
+        else {
+            // Loop over all files in given directory
+            for (File file : dir.listFiles()) {
+                // check for .txt extension
+                if (file.getName().endsWith(".txt")) {
+                    Scanner input = new Scanner(file);
 
-                while (input.hasNext()) {
-                    // toLowerCase to prevent duplicate keys of same word but different case
-                    String word = input.next().toLowerCase();
+                    // parse words out of String
+                    input.useDelimiter("[^a-zA-Z']+");
 
-                    if (wordCountMap.containsKey(word)) {
-                        int count = wordCountMap.get(word) + 1;
-                        wordCountMap.put(word, count);
+                    while (input.hasNext()) {
+                        // toLowerCase to prevent duplicate keys of same word but different case
+                        String word = input.next().toLowerCase();
+
+                        if(wordCountMap.containsKey(word)) {
+                            int count = wordCountMap.get(word) + 1;
+                            wordCountMap.put(word, count);
+                        }
+                        // create new key only if word does not exist in both maps
+                        else if (!wordCountMap.containsKey(word) && !mapToCompare.containsKey(word)) {
+                            wordCountMap.put(word, 1);
+                        }
                     }
                 }
             }
@@ -101,8 +104,8 @@ public class TextAnalysis {
     }
 
     // Convert HashMap into list of Entries to sort by value
-    public static List<Map.Entry<String, Integer>> convertHashMapToList(Map<String, Integer> map) {
-        List<Map.Entry<String, Integer>> listOfEntries = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+    private static List<Map.Entry<String, Integer>> convertHashMapToList(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> listOfEntries = new ArrayList<>(map.entrySet());
 
         // Implemented value comparator to sort entries based on values
         Collections.sort(listOfEntries, new Comparator<Map.Entry<String, Integer>>() {
@@ -114,12 +117,14 @@ public class TextAnalysis {
         return listOfEntries;
     }
 
-    public static void printResults(List<Map.Entry<String, Integer>> list) {
-        System.out.println("Top 10 Unique words in Bronte's works vs. Austen");
+    private static void printResults(List<Map.Entry<String, Integer>> list) {
+        System.out.println("Top 10 Unique words in Bronte's works vs. Austen:");
+        System.out.printf("%-16s%s", "\tWORD" , "OCCURRENCE\n");
+        System.out.println("\t-------------------------");
         for (int i = 0; i < 10; i++) {
             String word = list.get(i).getKey();
             int count = list.get(i).getValue();
-            System.out.println(word + " appeared " + count + " times");
+            System.out.printf("%-16s%s", "\t" + word, count + "\n");
         }
     }
 }
